8a7f078099ac2de31745c1ae18252bca29f1f2b5
jencekade <gaephuoc@gmail.com>
2017-11-10 11:16:09 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,12 +2,12 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
-
-//const data = [];
-
-
-IcebreakerResponse.BatchCreate = function(data) {
-  return data.map(function (emails) {
+/*take in an array which we call emailArray
+and them map it to get new array, in which we
+get each email in the array and send it to 
+the IcebreakerResponse function*/
+IcebreakerResponse.BatchCreate = function(emailArray) {
+  return emailArray.map(function (emails) {
     return new IcebreakerResponse(emails);
   });
 }
